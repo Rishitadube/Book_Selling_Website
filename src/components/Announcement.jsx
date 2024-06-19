1bef3595a216638b9cd8d6f57a65e8a853d20824
Dube <risdube@apac.corpdir.net>
2024-06-19 10:50:54 +0530

    some

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -12,7 +12,7 @@ const Container = styled.div`
 `;
 
 const Announcement = () => {
-  return <Container>Super Deal! Free Shipping on Orders Over 399/-</Container>;
+  return <Container>Super Super Deal! Free Shipping on Orders Over 199/-</Container>;
 };
 
 export default Announcement;
